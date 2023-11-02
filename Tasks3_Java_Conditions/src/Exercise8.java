646501b8f337f255f9882e3083ab84fef1cc0cde
Mihail Komarov <michaelkomarov03@gmail.com>
2023-11-02 15:44:03 +0200

    Added Exercise8 (error)

---
+++ Tasks3_Java_Conditions/src/Exercise8.java
@@ -0,0 +1,19 @@
+//Given a floating point number a.
+//If a number is not negative, then output its square to 3 decimal places,
+//otherwise its fourth power to 3 decimal places.
+
+import java.util.Scanner;
+
+public class Exercise8 {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)) {
+            double a = sc.nextDouble();
+
+            if(a >= 0) {
+                System.out.printf("%.3f%n", Math.pow(a, 2));
+            } else {
+                System.out.printf("%.3f%n", Math.pow(a, 4));
+            }
+        }
+    }
+}
