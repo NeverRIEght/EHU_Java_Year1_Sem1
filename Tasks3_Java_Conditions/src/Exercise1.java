ac03942342c9940b718214f44788a741686b86ac
Mihail Komarov <michaelkomarov03@gmail.com>
2023-11-02 14:47:19 +0200

    Fixed Exercise1.java name

--- Tasks3_Java_Conditions/src/Exercise1.java
+++ Tasks3_Java_Conditions/src/Exercise1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 //Given floating point numbers a and b.
 //If a is less than b, then output 7, otherwise 8.
 
-public class Excercise1 {
+public class Exercise1 {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             double a = sc.nextDouble();
