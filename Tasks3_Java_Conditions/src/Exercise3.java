c7fcb0466c36cfe3fd688c45a6526e86ac322ef6
Mihail Komarov <michaelkomarov03@gmail.com>
2023-11-02 14:50:38 +0200

    Added Exercise3

--- Tasks3_Java_Conditions/src/Exercise3.java
+++ Tasks3_Java_Conditions/src/Exercise3.java
@@ -1,2 +1,19 @@
+//Given integers a and b.
+//Output smaller of them.
+
+import java.util.Scanner;
+
 public class Exercise3 {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            if(a > b) {
+                System.out.println(b);
+            } else {
+                System.out.println(a);
+            }
+        }
+    }
 }
