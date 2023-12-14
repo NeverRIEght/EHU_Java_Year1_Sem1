d473387035400e772471cdd8710dbdb2ba53c78d
Mihail Komarov <michaelkomarov03@gmail.com>
2023-12-14 12:44:30 +0200

    Added Exercise 4.1.3

---
+++ Tasks4_Arrays/src/Exercise3.java
@@ -0,0 +1,22 @@
+public class Exercise3 {
+
+    /**
+     * method replaces all elements of a that greater than m with m
+     *
+     * @param a the given array
+     * @param m the given number
+     * @return the number of replacements
+     */
+    public static int getResult(int[] a, int m) {
+        int numberOfReplacements = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > m) {
+                a[i] = m;
+                numberOfReplacements++;
+            }
+        }
+
+        return numberOfReplacements;
+    }
+}
