ff440cb6c6ca1841214b497cfb17c60c397579f5
Mihail Komarov <michaelkomarov03@gmail.com>
2023-12-14 13:42:59 +0200

    Added Exercise 4.1.9

---
+++ Tasks4_Arrays/src/Exercise9.java
@@ -0,0 +1,18 @@
+public class Exercise9 {
+
+    /**
+     * @param a the given array
+     * @param c the given number
+     * @param d the given number
+     * @return the number of elements of the array a that belong to the segment [?, d]
+     */
+    public static int getResult(int[] a, int c, int d) {
+        int countOfAppropriateElements = 0;
+        for(int i = 0; i < a.length; i++) {
+            if(a[i] >= c && a[i] <= d) {
+                countOfAppropriateElements++;
+            }
+        }
+        return countOfAppropriateElements;
+    }
+}
