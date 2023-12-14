db3a44d03bd2d83eaed6044a73d0878ab9a1a84d
Mihail Komarov <michaelkomarov03@gmail.com>
2023-12-14 12:54:49 +0200

    Added Exercise 4.1.5

---
+++ Tasks4_Arrays/src/Exercise5.java
@@ -0,0 +1,26 @@
+public class Exercise5 {
+
+    /**
+     * method swaps the largest and smallest elements of array a
+     *
+     * @param a the given array, all its elements are distinct
+     */
+    public static void swapMinAndMax(int[] a) {
+        int minIndex = 0;
+        int maxIndex = 0;
+
+        for(int i = 0; i < a.length; i++) {
+            if(a[minIndex] > a[i]) {
+                minIndex = i;
+            }
+            if(a[maxIndex] < a[i]) {
+                maxIndex = i;
+            }
+        }
+        if(a[minIndex] != a[maxIndex]) {
+            a[minIndex] = a[minIndex] ^ a[maxIndex];
+            a[maxIndex] = a[maxIndex] ^ a[minIndex];
+            a[minIndex] = a[minIndex] ^ a[maxIndex];
+        }
+    }
+}
