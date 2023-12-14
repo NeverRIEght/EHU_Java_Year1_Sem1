13613fbbe6f728041bd5c11b91060007f1898254
Mihail Komarov <michaelkomarov03@gmail.com>
2023-12-14 13:09:23 +0200

    Added Exercise 4.1.6

---
+++ Tasks4_Arrays/src/Exercise6.java
@@ -0,0 +1,19 @@
+public class Exercise6 {
+
+    /**
+     * @param a the given array
+     * @return an array, its i-th element = 1 if a[i] > i, otherwise = 0
+     */
+    public static int[] getResult(int[] a) {
+        int[] binaryArr = new int[a.length];
+
+        for(int i = 0; i < a.length; i++) {
+            if(a[i] > i) {
+                binaryArr[i] = 1;
+            } else {
+                binaryArr[i] = 0;
+            }
+        }
+        return binaryArr;
+    }
+}
