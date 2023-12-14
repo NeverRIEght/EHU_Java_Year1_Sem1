0c31cdfa01665458e86dc8fcad96db5f45d98900
Mihail Komarov <michaelkomarov03@gmail.com>
2023-12-14 13:17:12 +0200

    Added Exercise 4.1.7

---
+++ Tasks4_Arrays/src/Exercise7.java
@@ -0,0 +1,29 @@
+public class Exercise7 {
+
+    /**
+     * @param a the given array, a.length <= 30
+     * @return the sum of elements of the array a whose indices are prime numbers
+     */
+    public static int getResult(int[] a) {
+
+        int sum = 0;
+
+        for(int i = 0; i < a.length; i++) {
+            if(i == 0 || i == 1) {
+                continue;
+            }
+
+            boolean isPrime = true;
+            for(int j = 2; j < i - 1; j++) {
+                if(i % j == 0) {
+                    isPrime = false;
+                }
+            }
+
+            if(isPrime) {
+                sum += a[i];
+            }
+        }
+        return sum;
+    }
+}
