8ae91203ffa5e08513d7be11e4f02ba6550d5fda
Mihail Komarov <michaelkomarov03@gmail.com>
2023-12-20 00:39:09 +0200

    Fixed Exercise 4.2.22

---
+++ Tasks4_Arrays/src/TwoDimensional/Exercise22.java
@@ -0,0 +1,19 @@
+package TwoDimensional;
+
+public class Exercise22 {
+    /**
+     * @param a m x n matrix
+     * @return the transpose matrix
+     */
+    public static int[][] transpose(int[][] a) {
+        int[][] b = new int[a[0].length][a.length];
+
+        for(int i = 0; i < b.length; i++) {
+            for(int j = 0; j < b[i].length; j++) {
+                b[i][j] = a[j][i];
+            }
+        }
+
+        return b;
+    }
+}
