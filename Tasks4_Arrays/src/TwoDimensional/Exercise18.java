adbcd4793f8dcad1697a4be5b0ad950a550353e1
Mihail Komarov <michaelkomarov03@gmail.com>
2023-12-20 00:25:14 +0200

    Added Exercise 4.2.18, error found in unit tests

---
+++ Tasks4_Arrays/src/TwoDimensional/Exercise18.java
@@ -0,0 +1,25 @@
+package TwoDimensional;
+
+public class Exercise18 {
+    /**
+     * @param a m x n matrix, n > 0
+     *          all elements of the matrix are distinct.
+     * @return column which contains the maximum value of the matrix
+     */
+    public static int columnOfMax(int[][] a) {
+
+        int max = a[0][0];
+        int column = 0;
+
+        for(int i = 0; i < a.length; i++) {
+            for(int j = 0; j < a[i].length - 1; j++) {
+                if(a[i][j] > max) {
+                    max = a[i][j];
+                    column = j;
+                }
+            }
+        }
+
+        return column;
+    }
+}
