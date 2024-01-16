0ffd8133fd04419b1e8a1233ee80755bfdd69976
Mihail Komarov <michaelkomarov03@gmail.com>
2024-01-16 22:58:04 +0200

    Exercise 7.2 (in progress)

--- Tasks7_OOP/src/Exercise2/Line.java
+++ Tasks7_OOP/src/Exercise2/Line.java
@@ -5,15 +5,63 @@ public class Line {
     private double k;
     private double b;
 
+    public Line(double k, double b) {
+        this.k = k;
+        this.b = b;
+    }
+
     public double ordinate(double x) {
         return k * x + b;
     }
 
     public Point intersection(Line line) {
+
+        //y = k1x + b1 (this)
+        //y = k2x + b2 (line)
+
+        //0 = (k1x - k2x) + (b1 - b2)
+        //0 = x(k1 - k2) + (b1 - b2)
+        //x(k1 - k2) = -(b1 - b2)
+        //x(k1 - k2) = b2 - b1
+        //x = (b2 - b1) / (k1 - k2)
+
+        if(this.getK() - line.getK() == 0) {
+            return null;
+        }
+
+        double interX = (line.getB() - this.getB()) / (this.getK() - line.getK());
+        double interY = this.getK() * interX + this.getB();
+
+        Point interPoint = new Point(interX, interY);
+        if(interPoint.isBelonging(this) && interPoint.isBelonging(line)) {
+            return interPoint;
+        }
+
         return null;
     }
 
     public Point intersection(Ray ray) {
+        if(this.getK() - ray.getLine().getK() == 0) {
+            return null;
+        }
+
+        double interX = (ray.getLine().getB() - this.getB()) / (this.getK() - ray.getLine().getK());
+        double interY = this.getK() * interX + this.getB();
+
+        Point interPoint = new Point(interX, interY);
+
+        if(interPoint.isBelonging(this) && interPoint.isBelonging(ray)) {
+            return interPoint;
+        }
+
         return null;
+    }
+
+    public double getK() {
+        return k;
+    }
+
+    public double getB() {
+        return b;
     }
 }
--- Tasks7_OOP/src/Exercise2/Point.java
+++ Tasks7_OOP/src/Exercise2/Point.java
@@ -3,15 +3,39 @@ public class Point {
     private double x;
     private double y;
 
-    public boolean isBelonging(Line line) {
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 
+    public boolean isBelonging(Line line) {
+        return this.y == line.getK() * this.x + line.getB();
     }
 
     public boolean isBelonging(Ray ray) {
-
+        if(!isBelonging(ray.getLine())) {
+            return false;
+        } else if(ray.isToRight()) {
+            return this.x >= ray.getX0();
+        } else {
+            return this.x <= ray.getX0();
+        }
     }
 
     public boolean isOver(Line line) {
+        return this.y > line.getK() * this.x + line.getB();
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        final double DELTA = 1.0e-9;
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Point other = (Point) obj;
+        return Math.abs(x - other.x) < DELTA && Math.abs(y - other.y) < DELTA;
     }
 }
