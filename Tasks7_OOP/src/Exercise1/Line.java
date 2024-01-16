11e03bb9f58e7fe00d91b4ea325e31b5d76dc282
Mihail Komarov <michaelkomarov03@gmail.com>
2024-01-16 20:01:33 +0200

    Exercise 7.1 (error)

---
+++ Tasks7_OOP/src/Exercise1/Line.java
@@ -0,0 +1,52 @@
+package Exercise1;
+
+public class Line {
+    //y = k * x + b
+    private double k;
+    private double b;
+
+    public Line() {
+
+    }
+
+    public Line(int k, int b) {
+        this.k = k;
+        this.b = b;
+    }
+
+    public boolean isParallel(Line line) {
+        return (this.getB() > line.getB() || this.getB() < line.getB()) && this.getK() == line.getK();
+    }
+
+    public boolean isPerpendicular(Line line) {
+        return this.getK() * line.getK() == -1;
+    }
+
+    public boolean isOver(Line line) {
+        return (this.getB() > line.getB() && this.getK() == line.getK());
+    }
+
+    public double angleOfSlope() {
+        return Math.atan(this.getK());
+    }
+
+    public double abscissa0() {
+        return -this.getB() / this.getK();
+    }
+
+    public double getK() {
+        return k;
+    }
+
+    public void setK(double k) {
+        this.k = k;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+}
