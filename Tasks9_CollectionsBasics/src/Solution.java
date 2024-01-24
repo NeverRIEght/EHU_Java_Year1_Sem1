1d5310134ca97db495f7d6ffe4e819e2579e0e65
Mihail Komarov <michaelkomarov03@gmail.com>
2024-01-24 14:12:40 +0200

    Exercise 9.1

--- Tasks9_CollectionsBasics/src/Solution.java
+++ Tasks9_CollectionsBasics/src/Solution.java
@@ -5,38 +5,74 @@ class Solution {
     private Solution() { }
 
     public static List<TestPack> createList(String str) {
-        //put your code here
-        return new ArrayList<>();
+        TestPack[] objects = decomposeString(str);
+        List<TestPack> output = new ArrayList<>();
+
+        Collections.addAll(output, objects);
+
+        return output;
     }
 
     public static Set<TestPack> createSet(String str) {
-        //put your code here
-        return new HashSet<>();
+        return new HashSet<>(createList(str));
     }
 
     public static SortedSet<TestPack> createSortedSet(String str) {
-        //put your code here
-        return new TreeSet<>();
+        return new TreeSet<>(createSet(str));
     }
 
     public static Queue<TestPack> createQueue(String str) {
-        //put your code here
-        return new LinkedList<>();
+        return new LinkedList<>(createList(str));
     }
 
     public static Queue<TestPack> createStack(String str) {
-        //put your code here
-        return new LinkedList<>();
+        TestPack[] objects = decomposeString(str);
+
+        TestPack[] inverseArray = new TestPack[objects.length];
+        for(int i = 0; i < objects.length; i++) {
+            inverseArray[i] = objects[objects.length - 1 - i];
+        }
+
+        Queue<TestPack> output = new LinkedList<>();
+
+        Collections.addAll(output, inverseArray);
+
+        return output;
     }
 
     public static Map<Integer, Result> createMap(String str) {
-        //put your code here
-        return new HashMap<>();
+        TestPack[] objects = decomposeString(str);
+
+        Map<Integer, Result> output = new HashMap<>();
+
+        for(TestPack elem : objects) {
+            output.put(elem.getId(), elem.getResult());
+        }
+
+        return output;
     }
 
     public static SortedMap<Integer, Result> createSortedMap(String str) {
-        //put your code here
-        return new TreeMap<>();
+        TestPack[] objects = decomposeString(str);
+        TreeMap<Integer, Result> output = new TreeMap<>();
+
+        for(TestPack elem : objects) {
+            output.put(elem.getId(), elem.getResult());
+        }
+
+        return output;
     }
 
+    private static TestPack[] decomposeString(String input) {
+        String[] strings = input.split(" ");
+        TestPack[] output = new TestPack[strings.length];
+
+        for(int i = 0; i < strings.length; i++) {
+            String[] parts = strings[i].split(";", 3);
+            Result res = new Result(Integer.parseInt(parts[1]), Integer.parseInt(parts[2]));
+            output[i] = new TestPack(Integer.parseInt(parts[0]), res);
+        }
+
+        return output;
+    }
 }
